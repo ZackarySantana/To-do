0b4bac1984e273ffa94dd856929ace074817f24c
Zackary Santana <zackzackyack@gmail.com>
2021-04-25 10:15:00 -0400

    Changed pallete for light mode

--- src/pages/App.svelte
+++ src/pages/App.svelte
@@ -23,16 +23,16 @@
 {#if !darkmode}
   <style>
     :root {
-      --parent-background: #14161a;
-      --background: #121212;
-      --primary: #eeeeee;
-      --secondary: #555;
+      --parent-background: #eeeeee;
+      --background: #dddddd;
+      --primary: #14161a;
+      --secondary: #121212;
       --third: #333;
 
-      --first_shadow: -2px -2px 3px rgba(255, 255, 255, 0.09);
-      --second_shadow: 3px 3px 3px rgba(0, 0, 0, 0.5);
+      --first_shadow: -2px -2px 3px rgba(255, 255, 255, 0.5);
+      --second_shadow: 3px 3px 3px rgba(0, 0, 0, 0.3);
 
-      --section_shadow: 0px 0px 30px rgba(0, 0, 0, 0.8);
+      --section_shadow: 0px 0px 10px rgba(0, 0, 0, 0.6);
     }
   </style>
 {:else}
